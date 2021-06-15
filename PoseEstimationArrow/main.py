41224aec52d270e9814db238c6dfed191a019f95
Lorenzo <lorenzo.pagnini@yahoo.it>
2021-06-15 18:27:25 +0200

    Split Calibration and pose estimation

--- PoseEstimationArrow/main.py
+++ PoseEstimationArrow/main.py
@@ -8,8 +8,9 @@ from PoseEstimationArrow.PoseEstimation import PoseEstimation
 from PoseEstimationArrow import utility
 
 NAME_WINDOW = "Calibration"
+CALIBRATION = False
 clicked_image = 0
-minimum_image = 5
+minimum_image = 20
 errors = np.zeros(minimum_image)
 
 # Prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
@@ -19,13 +20,14 @@ obj_p[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
 # Termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-calibration = cal.Calibration(obj_p, criteria=criteria)
+if CALIBRATION:
+    calibration = cal.Calibration(obj_p, criteria=criteria)
+else:
+    pose_estimation = PoseEstimation(obj_p, criteria, 'data.npz', draw_cube=False)
 
 mtx_mean = []
 dist_mean = []
 
-
-# pose_estimation = PoseEstimation(obj_p, criteria, 'data.npz', draw_cube=True)
 
 def callback_mouse(event, x, y, flag, param):
     global clicked_image, minimum_image, calibration
@@ -50,8 +52,9 @@ def main():
     global clicked_image, minimum_image, calibration, criteria, mtx_mean, dist_mean
 
     # Callback click mouse
-    cv.namedWindow(NAME_WINDOW)
-    cv.setMouseCallback(NAME_WINDOW, callback_mouse, calibration)
+    if CALIBRATION:
+        cv.namedWindow(NAME_WINDOW)
+        cv.setMouseCallback(NAME_WINDOW, callback_mouse, calibration)
 
     cap = cv.VideoCapture(0)
 
@@ -65,35 +68,45 @@ def main():
         success, frame = cap.read()
 
         if success:
+
             # Frame rate
             current_time = time.time()
             fps = np.divide(1, (current_time - previous_time))
             previous_time = current_time
             cv.putText(frame, f"FPS: {int(fps)}", (10, 35), cv.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
 
-            calibration.set_frame(frame.copy())
-            utility.draw_corners_chessboard(frame, criteria)
+            if CALIBRATION:
+                # Camera Calibration
+                calibration.set_frame(frame.copy())
+                utility.draw_corners_chessboard(frame, criteria)
 
-            if clicked_image < minimum_image:
+                if clicked_image < minimum_image:
 
-                cv.putText(frame, f"{clicked_image}/{minimum_image}", (530, 460), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 0),
-                           2)
-                cv.imshow(NAME_WINDOW, frame)
-                cv.waitKey(1)
+                    cv.putText(frame, f"{clicked_image}/{minimum_image}", (530, 460), cv.FONT_HERSHEY_PLAIN, 2,
+                               (0, 255, 0),
+                               2)
+                    cv.imshow(NAME_WINDOW, frame)
+                    cv.waitKey(1)
+
+                else:
+
+                    if len(mtx_arr) == 0:
+                        print(Fore.RED + f"Total error: {np.mean(errors)}")
+                        mtx_mean, dist_mean = utility.get_matrices(mtx_arr, dist_arr, minimum_image,
+                                                                   calibration.current_date)
+
+                    # Undistort
+                    dst = cv.undistort(frame, mtx_mean, dist_mean)
+
+                    cv.putText(dst, f"FPS: {int(fps)}", (10, 35), cv.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+                    cv.imshow('Image', dst)
+                    cv.imshow(NAME_WINDOW, frame)
+                    cv.waitKey(1)
 
             else:
-
-                if len(mtx_arr) == 0:
-                    print(Fore.RED + f"Total error: {np.mean(errors)}")
-                    mtx_mean, dist_mean = utility.get_matrices(mtx_arr, dist_arr, minimum_image,
-                                                               calibration.current_date)
-
-                # Undistort
-                dst = cv.undistort(frame, mtx_mean, dist_mean)  # , None, new_camera_mtx)
-
-                cv.putText(dst, f"FPS: {int(fps)}", (10, 35), cv.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
-                cv.imshow('Undistort', dst)
-                cv.imshow(NAME_WINDOW, frame)
+                # Pose Estimation
+                img = pose_estimation.start(frame)
+                cv.imshow("Image", img)
                 cv.waitKey(1)
 
 
