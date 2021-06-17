e40e42e65866b6fabffd271981975ce1c9550213
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-06-17 18:06:01 +0200

    Completed stereo open-cv

--- Stereo3D/Stereo.py
+++ Stereo3D/Stereo.py
@@ -1,60 +1,72 @@
-import cv2 as cv
+import cv2 as cv  # Version 3.4.14.53
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def load_data(name_file):
-    with np.load(name_file) as X:
-        mtx, dist = [X[i] for i in ('mtx', 'dist')]
+def drawlines(img_1, img_2, lines, pts1, pts2):
+    """ img1 - image on which we draw the epilines for the points in img2
+        lines - corresponding epilines """
 
-    return mtx, dist
+    r, c = img_1.shape
 
+    img_1 = cv.cvtColor(img_1, cv.COLOR_GRAY2BGR)
+    img_2 = cv.cvtColor(img_2, cv.COLOR_GRAY2BGR)
 
-def downsample_image(image, reduce_factor):
-    for i in range(0, reduce_factor):
-        row, col = image.shape[:2]
-        image = cv.pyrDown(image, dstsize=(col // 2, row // 2))  # Esegue un blurr e decampiona l'immagine
+    for r, pt1, pt2 in zip(lines, pts1, pts2):
+        color = tuple(np.random.randint(0, 255, 3).tolist())
 
-    return image
+        x0, y0 = map(int, [0, -r[2] / r[1]])
+        x1, y1 = map(int, [c, -(r[2] + r[0] * c) / r[1]])
 
+        img_1 = cv.line(img_1, (x0, y0), (x1, y1), color, 1)
+        img_1 = cv.circle(img_1, tuple(pt1), 5, color, -1)
+        img_2 = cv.circle(img_2, tuple(pt2), 5, color, -1)
 
-def main():
-    mtx, dist = load_data("data.npz")
-
-    img_1 = cv.imread("img1.jpg")
-    img_2 = cv.imread("img2.jpg")
-
-    height, width, _ = img_1.shape
-
-    new_matrix, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (width, height), 1)
-
-    img_1_undistort = cv.undistort(img_1, mtx, dist, newCameraMatrix=new_matrix)
-    img_2_undistort = cv.undistort(img_2, mtx, dist, newCameraMatrix=new_matrix)
-
-    img_1_down = downsample_image(img_1_undistort, 3)
-    img_2_down = downsample_image(img_2_undistort, 3)
-
-    win_size = 5  # Finestra da scorrere sull'immagine per normalizzare luminosità e migliorare la trama.
-    min_disp = -1  # Minima disparità consentita tra le 2 immagini.
-    max_disp = 63  # Massima disparità consentita tra le 2 immagini.
-    num_disp = max_disp - min_disp
-
-    stereo = cv.StereoSGBM_create(minDisparity=min_disp,
-                                  numDisparities=num_disp,
-                                  blockSize=5,
-                                  uniquenessRatio=5,
-                                  speckleWindowSize=5,
-                                  speckleRange=5,
-                                  disp12MaxDiff=1,
-                                  P1=8 * 3 * win_size ** 2,
-                                  P2=32 * 3 * win_size ** 2)
-
-    print("\nComputing the disparity map...")
-    disparity_map = stereo.compute(img_1_down, img_2_down)
-
-    plt.imshow(disparity_map, 'gray')
-    plt.show()
+    return img_1, img_2
 
 
-if __name__ == "__main__":
-    main()
+img_1 = cv.imread("img1.jpg", 0)
+img_2 = cv.imread("img2.jpg", 0)
+
+# ORB Descrictor
+orb = cv.ORB_create()
+
+kp1, des1 = orb.detectAndCompute(img_1, None)
+kp2, des2 = orb.detectAndCompute(img_2, None)
+
+# Create dictionary
+bf = cv.BFMatcher()
+matches = bf.knnMatch(des1, des2, k=2)
+
+good = []
+pts1 = []
+pts2 = []
+
+# Get good points
+for i, (m, n) in enumerate(matches):
+    if m.distance < 0.8 * n.distance:
+        good.append(m)
+        pts2.append(kp2[m.trainIdx].pt)
+        pts1.append(kp1[m.queryIdx].pt)
+
+pts1 = np.int32(pts1)
+pts2 = np.int32(pts2)
+
+# Calcola la matrice fondamentale tra i punti corrispondenti in due immagini.
+F, mask = cv.findFundamentalMat(pts1, pts2, cv.FM_LMEDS, None, None, None)
+pts1 = pts1[mask.ravel() == 1]
+pts2 = pts2[mask.ravel() == 1]
+
+# Disegna le linee epipolari per i punti 2
+lines1 = cv.computeCorrespondEpilines(pts2.reshape(-1, 1, 2), 2, F)
+lines1 = lines1.reshape(-1, 3)
+img_5, img_6 = drawlines(img_1, img_2, lines1, pts1, pts2)
+
+# Disegna le linee epipolari per i punti 2
+lines2 = cv.computeCorrespondEpilines(pts1.reshape(-1, 1, 2), 1, F)
+lines2 = lines2.reshape(-1, 3)
+img_3, img_4 = drawlines(img_2, img_1, lines2, pts2, pts1)
+
+plt.subplot(121), plt.imshow(img_5)
+plt.subplot(122), plt.imshow(img_3)
+plt.show()
