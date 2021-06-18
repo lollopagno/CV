36a041449f09f18a79b04f6521762b7d891d4046
Lorenzo <lorenzo.pagnini@yahoo.it>
2021-06-18 18:24:38 +0200

    Completed stereo 3d

--- Stereo3D/MyStereo.py
+++ Stereo3D/MyStereo.py
@@ -22,11 +22,32 @@ def downsample_image(image, reduce_factor):
     return image
 
 
-def main():
-    mtx, dist = load_data("data.npz")
+def create_output(vertices, colors, filename):
+    colors = colors.reshape(-1, 3)
+    vertices = np.hstack([vertices.reshape(-1, 3), colors])
 
-    img_1 = cv.imread("img1.jpg")
-    img_2 = cv.imread("img2.jpg")
+    ply_header = '''ply
+		format ascii 1.0
+		element vertex %(vert_num)d
+		property float x
+		property float y
+		property float z
+		property uchar red
+		property uchar green
+		property uchar blue
+		end_header
+		'''
+
+    with open(filename, 'w') as f:
+        f.write(ply_header % dict(vert_num=len(vertices)))
+        np.savetxt(f, vertices, '%f %f %f %d %d %d')
+
+
+def main():
+    mtx, dist = load_data("data/data.npz")
+
+    img_1 = cv.imread("images/img_1.png")
+    img_2 = cv.imread("images/img_2.png")
 
     height, width, _ = img_1.shape
 
@@ -58,6 +79,46 @@ def main():
 
     plt.imshow(disparity_map, 'gray')
     plt.show()
+
+    print("\nGenerating the 3D map...")
+    # Get new downsampled width and height
+    h, w, _ = img_2_down.shape
+
+    # Load focal length.
+    focal_length = np.load('data/FocalLength.npy')
+
+    # Perspective transformation matrix
+    # This transformation matrix is from the openCV documentation.
+    Q = np.float32([[1, 0, 0, -w / 2.0],
+                    [0, -1, 0, h / 2.0],
+                    [0, 0, 0, -focal_length],
+                    [0, 0, 1, 0]])
+
+    # This transformation matrix is derived from Prof. Didier Stricker's power point presentation on computer vision.
+    # Link : https://ags.cs.uni-kl.de/fileadmin/inf_ags/3dcv-ws14-15/3DCV_lec01_camera.pdf
+    Q2 = np.float32([[1, 0, 0, 0],
+                     [0, -1, 0, 0],
+                     [0, 0, focal_length * 0.05, 0],  # Focal length multiplication obtained experimentally.
+                     [0, 0, 0, 1]])
+
+    # Reproject points into 3D
+    points_3D = cv.reprojectImageTo3D(disparity_map, Q2)
+
+    colors = cv.cvtColor(img_1_down, cv.COLOR_BGR2RGB)
+
+    # Get rid of points with value 0 (i.e no depth)
+    mask_map = disparity_map > disparity_map.min()
+
+    # Mask colors and points.
+    output_points = points_3D[mask_map]
+    output_colors = colors[mask_map]
+
+    # Define name for output file
+    output_file = 'data/reconstructed.ply'
+
+    # Generate point cloud
+    print("\n Creating the output file... \n")
+    create_output(output_points, output_colors, output_file)
 
 
 if __name__ == "__main__":
