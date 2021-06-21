08217b63c42193d7617b5fa12b72d2bd155e955e
Lorenzo <lorenzo.pagnini@yahoo.it>
2021-06-21 18:07:01 +0200

    Minor improvement

--- Homography/main.py
+++ Homography/main.py
@@ -32,27 +32,30 @@ def get_descriptors(cards):
     return descriptor_total
 
 
-def find_card(img, descriptors, thres=15):
+def find_card(img, descriptors, threshold=15):
     global orb, matcher
 
     match_total = []
     result = -1
-    _, desc_2 = orb.detectAndCompute(img, None)
+    kp, desc_2 = orb.detectAndCompute(img, None)
 
-    for desc in descriptors:
-        matches = matcher.knnMatch(desc, desc_2, k=2)
-        good = []
+    try:
+        for desc in descriptors:
+            matches = matcher.knnMatch(desc, desc_2, k=2)
+            good = []
 
-        for m, n in matches:
-            if m.distance < 0.75 * n.distance:
-                good.append([m])
-        match_total.append(len(good))
+            for m, n in matches:
+                if m.distance < 0.75 * n.distance:
+                    good.append([m])
+            match_total.append(len(good))
+    except:
+        pass
 
     if len(match_total) != 0:
-        if max(match_total) > thres:
+        if max(match_total) > threshold:
             result = match_total.index(max(match_total))
 
-    return result
+    return result, kp
 
 
 cards, cards_name = load_images("cards")
@@ -72,10 +75,13 @@ while True:
         previous_time = current_time
 
         frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        id_card = find_card(frame_gray, descriptors, thres=25)
+        id_card, kp_input_card = find_card(frame_gray, descriptors)
         if id_card != -1:
             cv.putText(frame, cards_name[id_card], (450, 35), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 255), 2)
 
+        img_kp = cv.drawKeypoints(frame, kp_input_card, None, (255, 0, 0), 2)
+
         cv.putText(frame, f"FPS: {int(fps)}", (10, 35), cv.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+        cv.imshow("Keypoint", img_kp)
         cv.imshow("Current frame", frame)
         cv.waitKey(1)
