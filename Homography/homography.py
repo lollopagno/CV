12d96a7b08520b9907b91c5d7265333726307525
Lorenzo <lorenzo.pagnini@yahoo.it>
2021-06-25 16:17:41 +0200

    Merge homography

---
+++ Homography/homography.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+from Homography.HomographyModule import HomographyModule
+
+edge_detector = HomographyModule()
+
+path_img = "img2.jpg"
+
+img = cv.imread(f"imgs/{path_img}")
+img = cv.resize(img, (500, 500))
+img_canny = edge_detector.find_obj(img)
+edge_detector.get_obj()
+
+cv.imshow("Image", img)
+cv.imshow("Canny", img_canny)
+cv.waitKey(0)
