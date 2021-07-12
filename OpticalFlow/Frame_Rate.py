6c910dbc5be9f5100b8a2c6f62f6df931f211cf6
lollo_pagno <lorenzo.pagnini@yahoo.it>
2021-07-12 15:45:23 +0200

    Added the fps field to class

--- OpticalFlow/Frame_Rate.py
+++ OpticalFlow/Frame_Rate.py
@@ -9,9 +9,10 @@ class FrameRate:
     def __init__(self):
         self.current_time = 0
         self.previous_time = 0
+        self.fps = 0
 
     def run(self, img):
         self.current_time = time.time()
-        fps = np.divide(1, (self.current_time - self.previous_time))
+        self.fps = np.divide(1, (self.current_time - self.previous_time))
         self.previous_time = self.current_time
-        Utility.set_text(img, f"FPS: {int(fps)}", (610, 40), dim=1.5, color=Color.RED, thickness=4)
+        Utility.set_text(img, f"FPS: {int(self.fps)}", (610, 40), dim=1.5, color=Color.RED, thickness=4)
